b41d3dbe340658d4ce61053f0b2f90729e2b66e6
doctor3030 <dimamano@gmail.com>
2022-02-05 00:12:26 -0500

    v0.0.2 param message_cls in KafkaConfig changed to Dict[topic_name, message_class]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='kafka-app',
-    version="0.0.1",
+    version="0.0.2",
     author="Dmitry Amanov",
     author_email="",
     description="kafka application class",
